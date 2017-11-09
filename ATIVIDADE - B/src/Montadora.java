53faa8483e004f5f783475ac1b332253189a83d5
leandrovieira99 <leandroar9001@hotmail.com>
2017-11-09 16:15:52 -0200

    Prova POO 09112017

---
+++ ATIVIDADE - B/src/Montadora.java
@@ -0,0 +1,16 @@
+
+public class Montadora {
+	public String marca;
+	private int codigo;
+	public int estoque;
+	
+	public int VerificarEstoqueTotal(int estoque) {
+		int soma = 0;
+		return(soma += estoque);
+	}
+	
+    
+    
+	
+
+}
---
+++ ATIVIDADE - B/src/Veiculo.java
@@ -0,0 +1,20 @@
+import Montadora;
+
+public class Veiculo {
+
+	public static void main(String[] args) {
+     Veiculo v1 = new Veiculo();
+     
+    v1.marca(" gol");
+    v1.codigo(234);
+    v1.estoque("20")
+    
+     
+     
+     System.out.println(v1.marca);
+     System.out.println(v1.codigo);
+     System.out.println(v1.estoque);
+
+	}
+
+}
