d8ab714d2392f366709dda0bdbbf3b220a9e184d
leandrovieira99 <33494014+leandrovieira99@users.noreply.github.com>
2017-11-16 15:08:07 -0300

    Add files via upload

---
+++ Atividade_B1/src/Questao1/Split.java
@@ -0,0 +1,64 @@
+package Questao1;
+import javax.swing.*;
+import java.*;
+
+public class Split {
+    private boolean power;
+    private float temperatura;
+    private int velocidade;
+    private String modo;
+
+    public void menu(){
+        String x = JOptionPane.showInputDialog(" Split\nLigado: "+ power+"\nTemperatura: "+ temperatura + "\nVelocidade: "+ velocidade+"\nModo: "+modo);
+
+    }
+
+
+
+    public Split() {
+
+        this.power = false;
+        this.modo = "Frio";
+
+    }
+
+    public void setPower() {
+        if (power == false){
+         this.power = true;
+        }
+        else{
+            this.power = false;
+        }
+    }
+
+    public void setTemperatura(float temperatura) {
+        if (power == false) {
+            JOptionPane.showMessageDialog(null," Não pode executar pois está desligado");
+        }
+        else{
+            this.temperatura = temperatura;
+
+        }
+    }
+
+    public void setVelocidade(int velocidade) {
+        if (power == false) {
+            JOptionPane.showMessageDialog(null, " Não pode executar pois está desligado");
+        }
+        else{
+        this.velocidade = velocidade;
+        }
+    }
+
+
+
+    public void setModo() {
+        if (this.modo == "ventilado"){
+            this.modo = "Frio";
+        }
+        else{
+            this.modo = "Ventilado";
+
+        }
+    }
+}
